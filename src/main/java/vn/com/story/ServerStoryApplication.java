fbade69c06ded2b5867e8e1b187e128ee770db6a
ToDuyVuong <comc5288@gmail.com>
2024-01-10 00:40:06 +0700

    Initial commit

---
+++ src/main/java/vn/com/story/ServerStoryApplication.java
@@ -0,0 +1,13 @@
+package vn.com.story;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerStoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerStoryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/vn/com/story/ServerStoryApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.com.story;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerStoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
